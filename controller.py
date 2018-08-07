1b845abb19fff3744a1b7353511d6b74049cfb69
Oyewale Oyediran <oyewale.oyediran@getyourguide.com>
2018-08-07 23:36:01 +0200

    Reworked controller to basic settings

--- controller.py
+++ controller.py
@@ -1,13 +1,15 @@
-import os
-import pprint
-
-import math
 import pygame
+
+from utils.throttle import throttle
 
 MAX_FORCE = 5.0
 MIN_SPEED = 100
 MAX_SPEED = 300
 ALLOWANCE = 0.2
+
+STRAIGHT = 'straight'
+LEFT = 'left'
+RIGHT = 'right'
 
 
 class PS4Controller(object):
@@ -18,6 +20,9 @@ class PS4Controller(object):
     button_data = None
     hat_data = None
 
+    steering_orientation = STRAIGHT
+    force = 0.0
+
     def init(self):
         """Initialize the joystick components"""
 
@@ -27,40 +32,7 @@ class PS4Controller(object):
         self.controller.init()
 
     def get_controller_data(self):
-        distance = math.hypot(self.axis_data[0], self.axis_data[1])
-        force = abs(distance)
-
-        if force < ALLOWANCE:
-            return 0, 0
-
-        return force, self.calculate_angle()
-
-    def calculate_angle(self):
-        side_x, side_y = self.axis_data[0], self.axis_data[1]
-        if abs(side_x) < 0 or abs(side_y) < ALLOWANCE:
-            return 0
-
-        if side_x == 0:
-            return 0
-
-        tan_x = abs(side_y) / abs(side_x)
-        atan_x = math.atan(tan_x)
-
-        angle_x = atan_x * 180 / math.pi
-
-        if side_y < 0:
-            angle_x = angle_x * -1
-
-        if side_x < 0 and side_y < 0:
-            angle_x = 270 + abs(angle_x)
-        elif side_x < 0 < side_y:
-            angle_x = 270 - abs(angle_x)
-        elif side_x > 0 and side_y > 0:
-            angle_x = 90 + abs(angle_x)
-        else:
-            angle_x = 90 - abs(angle_x)
-
-        return angle_x
+        return self.force, self.steering_orientation
 
     def listen(self):
         """Listen for events to happen"""
@@ -68,25 +40,30 @@ class PS4Controller(object):
         if not self.axis_data:
             self.axis_data = {}
 
-        if not self.button_data:
-            self.button_data = {}
-            for i in range(self.controller.get_numbuttons()):
-                self.button_data[i] = False
-
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.JOYAXISMOTION:
                     self.axis_data[event.axis] = round(event.value, 2)
-                elif event.type == pygame.JOYBUTTONDOWN:
-                    self.button_data[event.button] = True
-                elif event.type == pygame.JOYBUTTONUP:
-                    self.button_data[event.button] = False
 
-                #
-                # print(self.get_controller_data())
-                # os.system('clear')
-                # pprint.pprint(self.button_data)
                 if 0 not in self.axis_data or 1 not in self.axis_data:
                     continue
-                print(self.get_controller_data())
-                # pprint.pprint(self.axis_data)
+
+                if self.axis_data[0] < -0.6:
+                    self.steering_orientation = LEFT
+                elif self.axis_data[0] > 0.6:
+                    self.steering_orientation = RIGHT
+                else:
+                    self.steering_orientation = STRAIGHT
+
+                if self.axis_data[1] < -0.5:
+                    self.force = (self.axis_data[1] + 0.5) * -2.
+                elif self.axis_data[1] > 0.5:
+                    self.force = (self.axis_data[1] - 0.5) * -2.
+                else:
+                    self.force = 0.0
+
+                self.move()
+
+    @throttle(0.2)
+    def move(self):
+        print(self.get_controller_data())
--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 from controller import PS4Controller
-from utils import throttle
-from utils.throttle import throttle2
 
 
 class TrainDrive(object):
     def __init__(self):
         self.ps4 = PS4Controller()
         self.ps4.init()
-        self.ps4.listen()
 
-    # @throttle2(1)
-    # def get_drive_data(self):
-    #     print("Test")
-    #     return self.ps4.get_controller_data()
+    def drive(self):
+        self.ps4.listen()
 
 
 if __name__ == "__main__":
     td = TrainDrive()
-    # while True:
-    #     print(td.get_drive_data())
+    td.drive()
